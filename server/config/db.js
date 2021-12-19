5fefb969c0a346167ae20ece232f90ecdee56a5f
SaiPrakashLopelli <saiprakashraolopelly@gmail.com>
2021-12-18 21:34:22 -0500

    assignment2

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+   // "URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://saiprakashlopelli:9695949321@mongodbsai.xbshr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}       //added database URI
